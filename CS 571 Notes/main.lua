dc85c5ddec7adf270717149707654360f633a966
Cameron Howell <crh0043@uah.edu>
2020-01-22 11:37:52 -0600

    First commit with notes

---
+++ CS 571 Notes/main.lua
@@ -0,0 +1,5 @@
+xx = display.contentCenterX;
+yy = display.contentCenterY;
+local ref = display.newRect(xx, yy, 100, 100);
+ref:setFillColor(1, 0, 0, 0.5);
+local a = display.newRect( 0, 0, 100, 100);
