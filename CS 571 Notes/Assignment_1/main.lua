1ae0b1eeb4c80452a3c396e5558b3065c872699c
Cameron Howell <crh0043@uah.edu>
2020-01-28 15:24:46 -0600

    Base code for CS571 Assignment #1

---
+++ CS 571 Notes/Assignment_1/main.lua
@@ -0,0 +1,53 @@
+--[[
+This is just the base code for assignment #1. Finish this and submit by February 4th.
+Author: Cameron Howell
+Class: CS571 Spring 2020
+Date: January 28, 2020
+Email: crh0043@uah.edu
+]]
+
+
+
+local balls={}
+local deltaX= 10
+local deltaY= -deltaX
+local ballRadius = 25
+
+local function setFillColorRandom(obj)
+  obj:setFillColor(math.random(), math.random(), math.random())
+end
+
+local function stopBall(event)
+  event.target.stopped = not event.target.stopped
+  return true
+end
+
+local function createBall(event)
+  local ball = display.newCircle( display.contentCenterX, display.contentCenterY, ballRadius )
+  setFillColorRandom(ball)
+  ball.deltaX = deltaX
+  ball.deltaY = deltaY
+  ball.ballRadius = ballRadius
+  table.insert(balls, ball)
+  ball:addEventListener("tap", stopBall)
+end
+
+Runtime:addEventListener("tap", createBall)
+
+local function update()
+  for _, ball in ipairs(balls) do
+    if not ball.stopped then
+      if ball.x + ball.deltaX > display.contentWidth - ball.ballRadius or ball.x + ball.deltaX < ballRadius then
+        ball.deltaX = -ball.deltaX
+      end
+      if ball.y + ball.deltaY > display.contentHeight - ball.ballRadius or ball.y + ball.deltaY < ballRadius then
+        ball.deltaY = -ball.deltaY
+      end
+
+      ball.x = ball.x + ball.deltaX
+      ball.y = ball.y + ball.deltaY
+    end
+  end
+end
+
+timer.performWithDelay( 15, update, 0 )
