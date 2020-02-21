c95df38a940b44af8d160c74e1ebf6539d2f25bc
Cameron Howell <crh0043@uah.edu>
2020-02-21 02:30:42 -0600

    Lab 3 save #2

--- CS 413 Notes/Lab3.s
+++ CS 413 Notes/Lab3.s
@@ -21,8 +21,8 @@
 
 main:
 @********************
-prompt:                     @Displays the welcome message. Contains a label so that some functions can just return to the
-@********************       @point where the prompt just asks for shape selection
+prompt:                     @Displays the welcome message.
+@********************
 
   LDR r0, =welcome
   BL printf
@@ -37,6 +37,7 @@ get_input:
   LDR r0, =selectionPrompt
   BL printf
 
+reenter:
   LDR r0, =charInputPattern
   LDR r1, =charInput
   BL scanf
@@ -48,39 +49,227 @@ get_input:
 @********************
 verify_selection:
 @********************
-  CMP r2, 'G'
-  LDREQ r1, =gum
-  CMP r2, 'P'
-  LDREQ r1, =peanuts
-  CMP r2, 'C'
-  LDREQ r1, =cracker
-  CMP r2, 'M'
-  LDREQ r1, =mandms
-  CMP r2, 'A'
+  CMP r2, #'G'
+  BNE p
+  LDREQ r3, =numGum
+  LDREQ r3, [r3]
+  CMPEQ r3, #0
+  LDRGE r1, =gum
+  LDRGT r6, =gum
+  MOVGT r5, #50
+  BGT ver
+  LDREQ r0, =outOfStock
+  BLEQ printf
+  B reenter
+
+p:
+  CMP r2, #'P'
+  BNE c
+  LDREQ r3, =numPeanuts
+  LDREQ r3, [r3]
+  CMPEQ r3, #0
+  LDRGE r1, =peanuts
+  LDRGT r6, =peanuts
+  MOVGT r5, #55
+  BGT ver
+  LDREQ r0, =outOfStock
+  BLEQ printf
+  B reenter
+
+c:
+  CMP r2, #'C'
+  BNE m
+  LDREQ r3, =numCrackers
+  LDREQ r3, [r3]
+  CMPEQ r3, #0
+  LDRGE r1, =cracker
+  LDRGT r6, =cracker
+  MOVGT r5, #65
+  BGT ver
+  LDREQ r0, =outOfStock
+  BLEQ printf
+  B reenter
+
+m:
+  CMP r2, #'M'
+  BNE a
+  LDREQ r3, =numMMs
+  LDREQ r3, [r3]
+  CMPEQ r3, #0
+  LDRGE r1, =mandms
+  LDRGT r6, =mandms
+  MOVGT r5, #100
+  BGT ver
+  LDREQ r0, =outOfStock
+  BLEQ printf
+  B reenter
+
+a:
+  CMP r2, #'A'
   BEQ secret
 
+  LDR r0, =invalidChoice
+  BL printf
+  B  reenter
+
+ver:
   LDR r0, =verifyInput
   BL printf
+
   LDR r0, =charInputPattern
   LDR r1, =correctInput
   BL scanf
+  CMP r0, #READERROR
+  BEQ readerror
   LDR r1, =correctInput
   LDRB r1, [r1]
 
-  CMP r1, 'Y'
+  CMP r1, #'Y'
   BEQ get_payment
-  CMP r1, 'N'
+  CMP r1, #'N'
   BEQ get_input
 
 @********************
 get_payment:
 @********************
+  MOV r7, #0
+  CMP r5, #100
+  LDREQ r0, =enterAmountDollar
+  LDRLT r0, =enterAmount
+  MOVLT r1, r5
+  BL printf
 
-B myexit
+  LDR r0, =moneyType
+  BL printf
+  B  cont
+
+payloop:
+  LDR r0, =notEnoughEntered
+  BL printf
+
+cont:
+  LDR r0, =charInputPattern
+  LDR r1, =moneyInput
+  BL  scanf
+  CMP r0, #READERROR
+  BEQ readerror
+  LDR r1, =moneyInput
+  LDRB r1, [r1]
+
+dime:
+  CMP r1, #'D'
+  BNE quarter
+  ADDEQ r7, r7, #10
+  CMPEQ r5, r7
+  BLE change_dispense
+  BGT payloop
+
+quarter:
+  CMP r1, #'Q'
+  BNE dollarbill
+  ADDEQ r7, r7, #25
+  CMPEQ r5, r7
+  BLE change_dispense
+  BGT payloop
+
+dollarbill:
+  CMP r1, #'B'
+  ADDEQ r7, r7, #100
+  CMPEQ r5, r7
+  BLE change_dispense
+  BGT payloop
+
+@********************
+change_dispense:
+@********************
+  LDR r0, =enoughEntered
+  BL printf
+
+  LDR r0, =dispensed
+  MOV r1, r6
+  BL printf
+
+  LDR r2, =gum
+  CMP r6, r2
+  LDREQ r1, =numGum
+  LDREQ r1, [r1]
+  SUBEQ r3, r1, #1
+  LDREQ r1, =numGum
+  STREQ r3, [r1, #0]
+
+  LDR r2, =cracker
+  CMP r6, r2
+  LDREQ r1, =numCrackers
+  LDREQ r1, [r1]
+  SUBEQ r3, r1, #1
+  LDREQ r1, =numCrackers
+  STREQ r3, [r1, #0]
+
+  LDR r2, =peanuts
+  CMP r6, r2
+  LDREQ r1, =numPeanuts
+  LDREQ r1, [r1]
+  SUBEQ r3, r1, #1
+  LDREQ r1, =numPeanuts
+  STREQ r3, [r1, #0]
+
+  LDR r2, =mandms
+  CMP r6, r2
+  LDREQ r1, =numMMs
+  LDREQ r1, [r1]
+  SUBEQ r3, r1, #1
+  LDREQ r1, =numMMs
+  STREQ r3, [r1, #0]
+
+  SUB r1, r7, r5
+  LDR r0, =changeReturn
+  BL printf
+
+@********************
+check_if_continue:
+@********************
+  LDR r0, =numGum
+  LDR r0, [r0]
+  LDR r1, =numMMs
+  LDR r1, [r1]
+  LDR r2, =numPeanuts
+  LDR r2, [r2]
+  LDR r3, =numCrackers
+  LDR r3, [r3]
+
+  ADD r4, r1, r0
+  ADD r4, r4, r2
+  ADDS r4, r4, r3
+  CMP r4, #0
+  BGT get_input
+  LDREQ r0, =emptyMachine
+  BL printf
+  B myexit
+
 @********************
 secret:
 @********************
-B myexit
+  LDR r0, =mmStock
+  LDR r1, =numMMs
+  LDR r1, [r1]
+  BL printf
+
+  LDR r0, =cStock
+  LDR r1, =numCrackers
+  LDR r1, [r1]
+  BL printf
+
+  LDR r0, =pStock
+  LDR r1, =numPeanuts
+  LDR r1, [r1]
+  BL printf
+
+  LDR r0, =gStock
+  LDR r1, =numGum
+  LDR r1, [r1]
+  BL printf
+  B get_input
+
 @********************
 readerror:
 @********************
@@ -121,13 +310,19 @@ selectionPrompt: .asciz "Enter item selection: Gum (G), Peanuts (P), Cheese Crac
 @Selection Verification Prompt
 @********************
 .balign 4
-verifyInput: .asciz "You selected %s. Is this correct? (Y/N):\n"
+invalidChoice: .asciz "Invalid option. Enter a proper option:\n"
+
+.balign 4
+verifyInput: .asciz "You selected %s. Is this correct? (Y/N): \n"
 
 @********************
 @Input Money Prompts
 @********************
 .balign 4
 enterAmount: .asciz "Enter at least %d cents for selection.\n"
+
+.balign 4
+enterAmountDollar: .asciz "Enter a dollar for this selection.\n"
 
 .balign 4
 moneyType: .asciz "Dimes (D), Quarters (Q), and Dollar Bills (B):\n"
@@ -160,6 +355,21 @@ outOfStock: .asciz "Sorry, but this machine is out of %s. Please make another se
 emptyMachine: .asciz "Mr. Zippy's Vending Machine is out of items! Goodbye!\n"
 
 @********************
+@Secret Menu Messages
+@********************
+.balign 4
+mmStock: .asciz "M&Ms - %d\n"
+
+.balign 4
+cStock: .asciz "Cheese Crackers - %d\n"
+
+.balign 4
+pStock: .asciz "Peanuts - %d\n"
+
+.balign 4
+gStock: .asciz "Gum - %d\n"
+
+@********************
 @All Variables and Input Formats
 @********************
 .balign 4
@@ -169,16 +379,16 @@ strInputPattern: .asciz "%[^\n]" @ Used to clear the input buffer for invalid in
 strInputError: .skip 100*4  @ User to clear the input buffer for invalid input.
 
 .balign 4
-charInputPattern: .asciz "%c"
+charInputPattern: .asciz "%s"
 
 .balign 4
-charInput: .byte ""
+charInput: .byte 0
 
 .balign 4
-correctInput: .byte ""
+correctInput: .byte 0
 
 .balign 4
-moneyInput: .byte ""
+moneyInput: .byte 0
 
 .balign 4
 itemSelected: .asciz ""
