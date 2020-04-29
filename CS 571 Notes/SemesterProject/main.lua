4c7aadc83c4a848b0f814bc3d3496235a135a288
Cameron Howell <crh0043@uah.edu>
2020-04-28 22:57:17 -0500

    [WIP] CS571 Project v0.2.alpha

--- CS 571 Notes/SemesterProject/main.lua
+++ CS 571 Notes/SemesterProject/main.lua
@@ -1,4 +1,8 @@
 --[[
-
-
+Author: Cameron Howell
+Class: CS 571 Spring 2020
+Assignment: Semester Project App
+Device: This program was made using the iPhone X prefab in Corona SDK
+Purpose: This program displays US COVID-19 data ranging from 1/22/20 to 4/28/20
 ]]
+composer.gotoScene( "stateScene", options )
--- CS 571 Notes/SemesterProject/state.lua
+++ CS 571 Notes/SemesterProject/state.lua
@@ -1,4 +1,4 @@
 --[[
-
-
+State class for all State instances
 ]]
+local State = {name = "", counties = {}, xPos = 0, yPos = 0,}
