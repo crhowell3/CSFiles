1afa4129ae0457a57ee892403cffd443ff0362ce
Cameron Howell <crh0043@uah.edu>
2020-08-24 22:46:34 -0500

    Added HW1 files for CS 102

---
+++ CS 102/HW1_1.0_Howell.c
@@ -0,0 +1,20 @@
+/**
+ * Author: Cameron Howell
+ * Class: CS 102 Fall 2020
+ * Date: August 24, 2020 
+ */
+#include <stdio.h>
+
+int main()
+{
+    // Classes marked 'N/A" do not have set meeting times or are completely asynchronous in nature
+    printf("COURSE\tDATE\tSTART\tEND\n");
+    printf("\nCS 102\tMW\t6:00\t7:20\n");
+    printf("CS 445\tN/A\tN/A\tN/A\n");
+    printf("CS 490\tMW\t9:40\t11:00\n");
+    printf("CS 499\tTR\t6:00\t7:20\n");
+    printf("CS 553\tN/A\tN/A\tN/A\n");
+    printf("CS 585\tN/A\tN/A\tN/A\n");
+
+    return 0;
+}
