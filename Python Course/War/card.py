fd7c435b3b11672adcf4f23454de29d259f8dd9e
Cameron Howell <crh0043@uah.edu>
2020-09-05 17:47:43 -0500

    Python classes for card game

--- Python Course/War/card.py
+++ Python Course/War/card.py
@@ -1,10 +1,6 @@
 '''
 Card class to define what a card object is for War
 '''
-import random
-suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
-         'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7,
           'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14}
 
