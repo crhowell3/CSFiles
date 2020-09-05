44845e03728981e41feed6bc94d162faa64a0205
Cameron Howell <crh0043@uah.edu>
2020-09-05 12:14:00 -0500

    Reformatted files to conform to pylint standards

---
+++ Python Course/pylinter_test.py
@@ -0,0 +1,16 @@
+'''
+A simple script to test pylint features
+'''
+
+
+def myfunc():
+    '''
+    Simple function for testing more pylint features
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+
+myfunc()
