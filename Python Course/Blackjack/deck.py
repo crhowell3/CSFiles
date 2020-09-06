2e80c04421d8b0bac90d54b54bea81f5d4d0c842
Cameron Howell <crh0043@uah.edu>
2020-09-05 21:51:03 -0500

    Full blackjack game in python

---
+++ Python Course/Blackjack/deck.py
@@ -0,0 +1,28 @@
+import random
+from card import Card
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven',
+         'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+
+
+class Deck:
+
+    def __init__(self):
+
+        self.deck = []
+
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit, rank))
+
+    def __str__(self):
+        deck_comp = ''
+        for card in self.deck:
+            deck_comp += '\n' + card.__str__()
+        return "The deck has: " + deck_comp
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        return self.deck.pop()
